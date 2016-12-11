41f2d79d816bb5341387abda9cfddd9b2866f6e1
Antoine R. Dumont (@ardumont) <antoine.romain.dumont@gmail.com>
2016-12-11 09:51:54 +0100

    Initialize a specific error and use it in the example method

--- spam-module.c
+++ spam-module.c
@@ -1,5 +1,22 @@
 #include <Python.h>
 
+static PyObject *SpamError;
+
+PyMODINIT_FUNC
+PyInit_spam(void)
+{
+    PyObject *m;
+
+    m = PyModule_Create(&spammodule); // FIXME: spammodule is not yet defined
+    if (m == NULL)
+        return NULL;
+
+    SpamError = PyErr_NewException("spam.error", NULL, NULL);
+    Py_INCREF(SpamError);
+    PyModule_AddObject(m, "error", SpamError);
+    return m;
+}
+
 static PyObject *
 spam_system(PyObject *self, PyObject *args)
 {
@@ -9,5 +26,9 @@ spam_system(PyObject *self, PyObject *args)
     if (!PyArg_ParseTuple(args, "s", &command))
         return NULL;
     sts = system(command);
+    if (sts < 0) {
+        PyErr_SetString(SpamError, "System command failed");
+        return NULL;
+    }
     return PyLong_FromLong(sts);
 }
